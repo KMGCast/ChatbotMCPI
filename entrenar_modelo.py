d64ef6d858897d275b52dbef147b2f65e0cde979
KMGCast <30113501@uaz.edu.mx>
2025-10-15 21:13:21 -0600

    Modelo actualizado

--- entrenar_modelo.py
+++ entrenar_modelo.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.linear_model import LogisticRegression
 import os
 import joblib
@@ -14,10 +16,24 @@ vectorizador=TfidfVectorizer(ngram_range=(1,2),max_features=1000)
 X=vectorizador.fit_transform(dataset['pregunta'])
 y=dataset['etiqueta']
 
-#Modelo + entrenamiento
-modelo=LogisticRegression(max_iter=1000, random_state=42)
-modelo.fit(X,y)
+#Separar datos: 
+X_train,X_test,y_train,y_test = train_test_split(X,y, test_size=0.2,stratify=y, random_state=42)
 
+#Hiperparámetros para el mejor modelo
+parametros = {
+    'C': [0.1, 1, 10],
+    'solver': ['liblinear', 'lbfgs']
+}
+
+grid = GridSearchCV(LogisticRegression(max_iter=1000, random_state=42),
+                    parametros, cv=5, scoring='accuracy')
+
+grid.fit(X_train, y_train)
+
+mejor_modelo = grid.best_estimator_
+
+#Modelo + entrenamiento
+y_pred=mejor_modelo.predict(X_test)
 #Guardar
-joblib.dump(modelo,'modelo/tutorMCPI_modelo.joblib')
+joblib.dump(mejor_modelo,'modelo/tutorMCPI_modelo.joblib')
 joblib.dump(vectorizador,'modelo/tutorMCPI_vectorizador.joblib')
